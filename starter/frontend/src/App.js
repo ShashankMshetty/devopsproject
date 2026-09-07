fa6f37d7cbad4100b7be2d80648d19f6e5bd535a
ShashankMshetty <s.mshetty32@gmail.com>
2026-09-07 15:05:10 +0530

    trigger frontend CI

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -3,6 +3,11 @@ import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
+
+/* This is a
+   multi-line comment */   
+
+   
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
